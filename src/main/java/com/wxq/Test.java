25952cec31d899a3bb7527ffdcee78db7fc61fd6
wenxiangqin <wenxiangqin.wxq@alibaba-inc.com>
2022-04-11 09:34:43 +0800

    test commit

---
+++ src/main/java/com/wxq/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Alibaba.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package com.wxq;
+
+/**
+ *
+ * @author wenxiangqin
+ * @version $Id: Test.java, v 0.1 2022-04-11 9:34 上午 wenxiangqin Exp $$
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
